83b416a924d06e9d75691e15bd25f69d43c57b1d
James Smith <james@floppy.org.uk>
2011-12-22 17:32:47 +0000

    Encode URLs correctly during login and logout. EX-2115

--- lib/my_amee_helper.rb
+++ lib/my_amee_helper.rb
@@ -1,11 +1,11 @@
 module MyAmeeHelper
 
   def logout_url
-    "#{MyAmee::Config.get['url']}/logout?next=#{controller.current_url}"
+    "#{MyAmee::Config.get['url']}/logout?next=#{CGI.escape(controller.current_url)}"
   end
 
   def login_url
-    "#{MyAmee::Config.get['url']}/login?next=#{controller.current_url}"
+    "#{MyAmee::Config.get['url']}/login?next=#{CGI.escape(controller.current_url)}"
   end
 
   def preferences_url
